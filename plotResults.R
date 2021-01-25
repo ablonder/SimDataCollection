20ede999b2c2a222b069abc596ac69d4055247bd
ablonder@oberlin.edu <afblonder@ucdavis.edu>
2021-01-25 15:29:10 -0800

    added options to plotResults

--- plotResults.R
+++ plotResults.R
@@ -18,15 +18,20 @@ library(gridExtra)
 #   can be used to color the graph or for the x-axis if included in params as "category"
 # testlabels - vector of labels for the y-axis of each graph, should be one or the same length as testvars
 # catlabels - vector of labels for the colors of each graph, should be one or the same length as testvars
-# scatter - whether a scatter plot is returned, or the data is averaged over to make a barplot/linegraph
+# scatter - whether a scatter plot is returned, or the data is turned into a barplot/linegraph
 # linelabels - vector of labels for the line patterns of each graph, should be one or the same length as testvars
 # shapelables - vector of labels for the point shapes of each graph, should be one or the same length as testvars
 # savelv - how many parameter combinations should be included in the same facetted plot, maximum 4
 # pointsize - whether points should be scaled according to the number of values averaged to create them
-# errorbars - whether linegraphs or barplots should include error bars
+# errorbars - whether plots should include error bars
+# colorlegend, linelegend, shapelegend - whether to actually display the legends for each attribute
+# aggregate - whether entries with the same provided params are averaged over or all displayed
+# fitline - whether a scatterplot has a line of fit
+# width, height - dimensions for the graph created, default to the size of the current plotting window
 plotResults = function(dtable, params, fname, testvars, testlabels, catlabels, scatter = F, 
                        linelabels = c(), shapelabels = c(), savelv = 2, pointsize = F, errorbars = F,
-                       colorlegend = T, linelegend = T, shapelegend = T, aggregate = T){
+                       colorlegend = T, linelegend = T, shapelegend = T, aggregate = T, fitline = F,
+                       width = par("din")[1], height = par("din")[2]){
   if(length(params) == savelv){
     # loop through each set of measures to crete the corresponding plots
     for(v in 1:length(testvars)) {
@@ -47,49 +52,56 @@ plotResults = function(dtable, params, fname, testvars, testlabels, catlabels, s
       # I'm going to have an index here to increment
       j = 1
       # if the data is going to be split into shapes, use the first category for that
+      sleg = F
       if(length(shapelabels) > 0){
         plotd$s = factor(plotd[, cats[1]])
         j = j+1
-        # also, if there's only one shape label given use that
-        if(length(shapelabels) == 1){
-          slab = shapelabels[1]
-        } else {
-          # otherwise, use the label corresponding to this test variable
-          slab = shapelabels[v]
+        # also if we're actually doing a shape legend, create that
+        if(shapelegend){
+        # if there's only one shape label given use that
+          if(length(shapelabels) == 1){
+            slab = shapelabels[1]
+          } else {
+            # otherwise, use the label corresponding to this test variable
+            slab = shapelabels[v]
+          }
+          sleg = guide_legend(title = slab)
         }
-      } else {
-        slab = ""
       }
       # if there are more categories left and this is going to be split into linetypes, do so
+      lleg = F
       if(length(cats) >= j & length(linelabels) > 0){
         plotd$l = factor(plotd[, cats[j]])
         j = j+1
-        # also create the line legend
-        if(length(linelabels) == 1){
-          llab = linelabels[1]
-        } else {
-          llab = linelables[v]
+        # also create the line legend if there's going to be one
+        if(linelegend){
+          if(length(linelabels) == 1){
+            llab = linelabels[1]
+          } else {
+            llab = linelables[v]
+          }
+          lleg = guide_legend(title = llab)
         }
-      } else {
-        llab = ""
       }
       # if there are more categories left, use them to color the graph
+      cleg = F
       if(length(cats) >= j) {
         for(i in j:length(cats)){
           plotd$c = paste(plotd$c, plotd[, cats[i]])
         }
-        # also set the color label
-        if(length(catlabels) == 1){
-          clab = catlabels[1]
-        } else {
-          clab = catlables[v]
+        # also set the color label if there's going to be a legend
+        if(colorlegend){
+          if(length(catlabels) == 1){
+            clab = catlabels[1]
+          } else {
+            clab = catlables[v]
+          }
+          cleg = guide_legend(title = clab)
         }
-      } else {
-        clab = ""
       }
       # turn the color into a factor
       plotd$c = factor(plotd$c)
-      # if this isn't a scatter plot average the relevant measures so there aren't duplicates
+      # if the data isn't being aggregated, average the relevant measures so there aren't duplicates
       if(aggregate){
         plotd = summarize(group_by(group_by_at(plotd, params[1:(length(params)-2)]), x, c, s, l, add = T),
                           N = length(measure), mean = mean(measure, na.rm = T),
@@ -97,22 +109,6 @@ plotResults = function(dtable, params, fname, testvars, testlabels, catlabels, s
         plotd = as.data.frame(plotd)
       } else {
         plotd = mutate(plotd, N = 1, mean = measure, sd = 0, se = 0)
-      }
-      # toggle legends - TODO - integrate this so it only creates legends if the category actually exists
-      if(colorlegend){
-        cleg = guide_legend(title=clab)
-      } else {
-        cleg = F
-      }
-      if(linelegend){
-        lleg = guide_legend(title = llab)
-      } else {
-        lleg = F
-      }
-      if(shapelegend){
-        sleg = guide_legend(title = slab)
-      } else {
-        sleg = F
       }
       # also toggle error bars by getting rid of standard error
       if(!errorbars){
@@ -124,9 +120,11 @@ plotResults = function(dtable, params, fname, testvars, testlabels, catlabels, s
         if(scatter){
           plot = ggplot(plotd, aes(x = x, y = measure, colour = c, shape = s)) + geom_point() +
             ylab(testlabels[v]) + xlab(params[length(params)-1]) + labs(colour = params[length(params)]) +
-            guides(colour=cleg, shape=sleg) + geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.1) +
-            geom_smooth(method = 'lm', se = F)
-          ## TODO - give it the option to turn off the geom_smooth()
+            guides(colour=cleg, shape=sleg) + geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.1)
+          # adds a fitline if that option is turned on
+          if(fitline){
+            plot = plot + geom_smooth(method = 'lm', se = F)
+          }
         } else {
         # otherwise turn this into a linegraph
           # if the points are scaled by number of points, do that here
@@ -165,7 +163,8 @@ plotResults = function(dtable, params, fname, testvars, testlabels, catlabels, s
         plot = plot + facet_grid(rows = vars(plotd[, params[(length(params)-2)]]),
                           cols = vars(plotd[, params[(length(params)-3)]]))
       }
-      ggsave(paste(fname, testvars[v], ".png", sep = ""), plot = plot)
+      # save the plot with the given file name and options to change height and width
+      ggsave(paste(fname, testvars[v], ".png", sep = ""), plot = plot, width = width, height = height)
     }
   } else {
     # otherwise, recurse on all values of this parameter
@@ -176,7 +175,8 @@ plotResults = function(dtable, params, fname, testvars, testlabels, catlabels, s
       plotResults(dtable[dtable[, params[1]] == v, ], params[-1], f, testvars, testlabels, catlabels,
                   scatter, linelabels = linelabels, shapelabels = shapelabels, savelv = savelv,
                   colorlegend = colorlegend, linelegend = linelegend, shapelegend = shapelegend,
-                  errorbars = errorbars, pointsize = pointsize, aggregate = aggregate)
+                  errorbars = errorbars, pointsize = pointsize, aggregate = aggregate, fitline = fitline,
+                  width = width, height = height)
     }
   }
 }
