968d236a905eaa3c4931e385256b57c88cc93a39
ablonder@oberlin.edu <afblonder@ucdavis.edu>
2021-02-22 14:41:08 -0800

    added header reading and flexible combining to combineFiles

--- combineFiles.R
+++ combineFiles.R
@@ -1,6 +1,7 @@
 library(stringr)
 
-combineFiles = function(fnames, paramIDs=c(), endstrings=c(), paramnames=c(), sep = ",", filetypes = c("")){
+combineFiles = function(fnames, paramIDs=c(), endstrings=c(), paramnames=c(), sep = ",", filetypes = c(""),
+                        readheader = F){
   dtable = data.frame()
   for(f in 1:length(fnames)){
     # just combine the ones that have the provided strings (or, if no strings have been provided, combine all)
@@ -24,8 +25,8 @@ combineFiles = function(fnames, paramIDs=c(), endstrings=c(), paramnames=c(), se
         }
         # loop through all the splits in order
         for(s in 1:length(splitorder)){
-          # only use it if it's actually a parameter, not just an end string
-          if(splitorder[s] < length(params)+1){
+          # only use it if it's actually a parameter, not just an end string (and also not already a column)
+          if(splitorder[s] < length(params)+1 & !is.element(params[splitorder[s]], names(d))){
             # grab the value between the end of this split and the beginning of the next
             val = substr(fnames[f], ends[splitorder[s]]+1, starts[splitorder[s+1]]-1)
             if(!is.na(as.numeric(val))) { val = as.numeric(val) }
@@ -34,7 +35,33 @@ combineFiles = function(fnames, paramIDs=c(), endstrings=c(), paramnames=c(), se
           }
         }
       }
-      # now merge this dataframe with the previous
+      # once that's done, I can add the remaining parameters straight from the header
+      if(readheader){
+        header = read.table(text = gsub(",|:", "\n", readLines(fnames[f], n = 1)), sep = "=", strip.white = T, skip = 1)
+        # check each row to add the value to the table
+        for(r in 1:nrow(header)){
+          if(!is.element(toString(header[r,1]), names(d))){
+            if(is.numeric(header[r,2])){
+              d[,toString(header[r,1])] = as.numeric(header[r,2])
+            } else{
+              d[,toString(header[r,1])] = toString(header[r,2])
+            }
+          }
+        }
+      }
+      # now merge this dataframe with the previous, accounting for any columns they may not share
+      for(n in names(dtable)){
+        if(!is.element(n, names(d))){
+          d[,n] = NA
+        }
+      }
+      if(nrow(dtable) > 0){
+        for(n in names(d)){
+          if(!is.element(n, names(dtable))){
+            dtable[,n] = NA
+          }
+        }
+      }
       dtable = rbind(dtable, d)
     }
   }
